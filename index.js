e804e06af731cb295fc062350c11bc861404bfd3
Seil Cho <seilcho7@hotmail.com>
2019-04-01 18:45:06 -0400

    adds fortunes

---
+++ index.js
@@ -0,0 +1,7 @@
+const fortunes = [
+    "It is easier to run down a hill than up one.",
+    "If God had meant for us to be naked, we would have been born that way.",
+    "All true wisdom is found on T-shirts.",
+    "If God had a beard, he'd be a UNIX programmer.",
+    "If you refuse to accept anything but the best you very often get it."
+];
