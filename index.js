a192dd84e3e12507877f8dd36af6d716689c8ab0
Seil Cho <seilcho7@hotmail.com>
2019-04-02 08:53:07 -0400

    shows random fortunes on screen

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
 const fortunes = [
     "It is easier to run down a hill than up one.",
     "If God had meant for us to be naked, we would have been born that way.",
@@ -5,3 +10,15 @@ const fortunes = [
     "If God had a beard, he'd be a UNIX programmer.",
     "If you refuse to accept anything but the best you very often get it."
 ];
+
+async function getFortune(req, res) {
+    const randomFortune = await fortunes[Math.floor(Math.random()*5)];
+    console.log(randomFortune);
+    res.json(randomFortune);
+}
+
+app.get('/fortune', getFortune);
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
