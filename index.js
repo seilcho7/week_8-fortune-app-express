746106e1e6cebe5e64450e692a11d624cf597dc4
Seil Cho <seilcho7@hotmail.com>
2019-04-02 10:03:02 -0400

    commented out unused functions

--- index.js
+++ index.js
@@ -5,19 +5,19 @@ const port = 3000;
 
 const axios = require('axios');
 
-const fortunes = [
-    "It is easier to run down a hill than up one.",
-    "If God had meant for us to be naked, we would have been born that way.",
-    "All true wisdom is found on T-shirts.",
-    "If God had a beard, he'd be a UNIX programmer.",
-    "If you refuse to accept anything but the best you very often get it."
-];
+// const fortunes = [
+//     "It is easier to run down a hill than up one.",
+//     "If God had meant for us to be naked, we would have been born that way.",
+//     "All true wisdom is found on T-shirts.",
+//     "If God had a beard, he'd be a UNIX programmer.",
+//     "If you refuse to accept anything but the best you very often get it."
+// ];
 
-async function getFortune(req, res) {
-    const randomFortune = await fortunes[Math.floor(Math.random()*5)];
-    console.log(randomFortune);
-    res.json(randomFortune);
-}
+// async function getFortune(req, res) {
+//     const randomFortune = await fortunes[Math.floor(Math.random()*5)];
+//     console.log(randomFortune);
+//     res.json(randomFortune);
+// }
 
 // app.get('/fortune', getFortune);
 
